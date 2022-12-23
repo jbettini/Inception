11419819434176ed4b9e3502c374a57f136c6e2e
jbettini <jbettini@exemple.com>
2022-12-23 22:03:16 +0100

    wow

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -4,29 +4,31 @@ if [ ! -d "var/lib/mysql/${DB_NAME}" ]; then
 	sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 	rc-service mariadb setup
 	rc-service mariadb start
-	mysql -u root
+	chmod 777 /var/run/mysqld
+	mysql -u root << EOF 
+USE mysql;
 
-	USE mysql;
+#Reload privilege tables
+FLUSH PRIVILEGES;
 
-	#Reload privilege tables
-	FLUSH PRIVILEGES;
+#Remove test Database
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
 
-	#Remove test Database
-	DROP DATABASE IF EXISTS test;
-	DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+#Delete anonymous user
+#DELETE FROM mysql.global_priv WHERE User='';
 
-	#Delete anonymous user
-	DELETE FROM mysql.global_priv WHERE User='';
+#Remove Remote root
+#DELETE FROM mysql.global_priv WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 
-	#Remove Remote root
-	DELETE FROM mysql.global_priv WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-	
-	#Configure our database
-	CREATE DATABASE ${DB_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
-	CREATE USER '${DB_USER}'@'%' IDENTIFIED by '${DB_PASS}';
-	GRANT ALL PRIVILEGES ON wordpress.* TO '${DB_USER}'@'%';
-	ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT}';
-	FLUSH PRIVILEGES;
-	EOF
-		rc-service mariadb restart
+#Configure our database
+CREATE DATABASE ${DB_NAME}  CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '${DB_USER}'@'%' IDENTIFIED by '${DB_PASS}';
+GRANT ALL PRIVILEGES ON wordpress.* TO '${DB_USER}'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT}';
+FLUSH PRIVILEGES;
+
+EOF
+	rc-service mariadb restart
 fi
